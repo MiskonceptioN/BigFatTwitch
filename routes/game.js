d18c115083af08a5a6b1955121df6e0742a392b1
Danny Valentine <me@danny-valentine.com>
2024-02-03 22:57:11 +0000

    Add inGame when populating users

--- routes/game.js
+++ routes/game.js
@@ -30,7 +30,7 @@ router.get("/join", checkAuthenticated, (req, res) => {
 		const game = await Game.findOne({code: gameCode}).populate({
 			path: 'teams.players',
 			model: User,
-			select: '_id twitchId displayName profileImageUrl broadcasterType chatColour twitchChatColour customChatColour',
+			select: '_id twitchId displayName profileImageUrl broadcasterType chatColour twitchChatColour customChatColour inGame',
 			foreignField: 'twitchId',
 		});
 
