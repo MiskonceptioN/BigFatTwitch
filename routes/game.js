4a4744c7bc1aac112476afd672d3e0971947ca99
Danny Valentine <me@danny-valentine.com>
2024-02-03 23:13:34 +0000

    Fetch game data when rendering the waiting-room page

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { checkAuthenticated, generateGameCode, createErrorHTML } = require("../helpers");
+const { checkAuthenticated, generateGameCode, createErrorHTML, saveSession } = require("../helpers");
 
 // Pull in socket.io
 const io = require('../app');
@@ -83,11 +83,59 @@ router.get("/join", checkAuthenticated, (req, res) => {
 	}
 });
 
-router.get("/waiting-room", checkAuthenticated, (req, res) => {
+router.get("/waiting-room", checkAuthenticated, async (req, res) => {
 	const failureMessage = req.flash("error")[0]; // Retrieve the flash message
 	const successMessage = req.flash("success")[0]; // Retrieve the flash message
+	let isContestant = false;
 
-	res.render("game/waiting-room", {user: req.user, failureMessage, successMessage});
+	// Check user has inGame in their user object
+	if (!req.user.inGame) {
+		req.flash("error", "You're not in a game!");
+		res.redirect("join");
+		return;
+	}
+
+	// Check logged in user is a player
+	const game = await Game.findOne({code: req.user.inGame}).populate({
+		path: 'teams.players',
+		model: User,
+		select: '_id twitchId displayName profileImageUrl broadcasterType chatColour twitchChatColour customChatColour inGame',
+		foreignField: 'twitchId',
+	});
+
+	// Redirect the user if the game doesn't exist
+	if (!game) {
+		req.flash("error", "Game not found!");
+		res.redirect("join");
+		return;
+	}
+
+	// Check to see if the user joining the game is one of the players
+	for (let team of game.teams) {
+		for (let player of team.players) {
+			if (player.twitchId == req.user.twitchId) {
+				isContestant = true;
+
+				// Update the session to include the game code
+				req.session.passport.user.doc = {
+					...req.session.passport.user.doc,
+					inGame: req.user.inGame,
+					game
+				};
+				
+				try {
+					await saveSession(req);
+					return res.render("game/waiting-room", {user: req.user, game, failureMessage, successMessage});
+				} catch (err) {
+					return res.render("game/waiting-room", {user: req.user, game, failureMessage, successMessage});
+				}
+			}
+		}
+	}
+
+	if (!isContestant) {
+		return res.redirect("audience");
+	}
 })
 
 module.exports = router;
