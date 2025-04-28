11519800892d0577a5e794d06c2d49a502652872
Danny Valentine <social@dannyvalz.com>
2025-04-29 00:15:51 +0100

    Improve schema structure

--- models/questionModel.js
+++ models/questionModel.js
@@ -4,16 +4,18 @@ const timestamps = require("mongoose-timestamp");
 
 // Define the Question schema
 const questionSchema = new mongoose.Schema({
+	game: { type: String, ref: 'Game', foreignField: 'code' }, // Reference to the game code
+	round: { type: Number, required: true }, // Round number
+	order: { type: Number, required: true }, // Question order within the round
 	question: { type: String, required: true },
 	answer: { type: String, required: true },
-	contestantAnswers: { type: Object, default: {} },
+	contestantAnswers: [{
+		contestantId: { type: String, required: true },
+		answer: { type: String, required: true },
+		points: { type: Number, default: 0 },
+	}],
 	status: { type: String, enum: ["pending", "in-progress", "played"], default: "pending" },
 	type: { type: String, enum: ["text", "video", "image"], default: "text" },
-//	 game: { type: mongoose.Schema.Types.ObjectId, ref: "Game", required: true },
-	game: { type: "String", required: true },
-	round: { type: Number, required: true },
-	order: { type: Number, required: true },
-//	 winner: { type: mongoose.Schema.Types.ObjectId, ref: "Team" }
 });
 // questionSchema.plugin(timestamps);
 questionSchema.set('timestamps', true);
