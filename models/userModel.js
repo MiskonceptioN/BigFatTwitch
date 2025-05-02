5c0094f5bb7715114889c9fd73048fe208cbc04a
Danny Valentine <social@dannyvalz.com>
2025-05-02 17:29:22 +0100

    Minor refactor for maintainability

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const findOrCreate = require('mongoose-findorcreate');
 
+const onePxPng = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII=";
+
 // Define the User schema
 const userSchema = new mongoose.Schema({
   twitchId: { type: String, required: true },
@@ -17,7 +19,8 @@ const userSchema = new mongoose.Schema({
   customChatColour: {type: String, default: "#000000"},
   inGame: {type: String, default: ""},
   loggedOutOf: {type: String, default: ""},
-  answer: {type: String, default: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII="}, // base64 of the most recent answer
+  answer: {type: String, default: onePxPng}, // base64 of the most recent answer
+  // lastAnswerId: { type: mongoose.Schema.Types.ObjectId, ref: "Answer" }
 });
 userSchema.plugin(findOrCreate);
 userSchema.set('timestamps', true);
