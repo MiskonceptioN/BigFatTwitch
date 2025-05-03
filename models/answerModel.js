ccf2b75d23d51cbfad12f1891324fd57ede300ca
Danny Valentine <social@dannyvalz.com>
2025-05-03 20:40:01 +0100

    Remove unneeded comment

--- models/answerModel.js
+++ models/answerModel.js
@@ -3,7 +3,6 @@ const timestamps = require("mongoose-timestamp");
 
 const answersSchema = new mongoose.Schema({
 	questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', foreignField: '_id', required: true }, // Reference to the question
-	// questionId: { type: String, ref: 'Question', foreignField: 'id', required: true }, // Reference to the question
 	game: { type: String, ref: 'Game', required: true }, // Reference to the game code
 	contestant: { type: String, ref: 'User', required: true, refPath: 'twitchId' }, // Reference to the contestant's Twitch ID
 	answer: { type: String, required: true },
