5c1e5ed416c0e6a559cd3a4f0fb59c9b018be9cb
Danny Valentine <social@dannyvalz.com>
2025-07-21 23:06:12 +0100

    Remove unnecessary comments

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,7 @@ router.get("/debug", async (req, res) => {
 	const currentlyRunningGame = await checkForRunningGame();
 
 	const questions = [];
-	// const questions = await Question.find({});
 	// Pull all the questions from the Game model
-	// const questions = await Game.find({}).sort({createdAt: -1}).limit(1).select("questions");
 	const allGames = await Game.find({}).populate({
 		path: 'questions',
 		// select: '_id game question answer round order status',
@@ -43,8 +41,6 @@ router.get("/debug", async (req, res) => {
 			questions.push(question);
 		});
 	});
-
-	// console.log(questions);
 
 	res.render("debug", {user: req.user, game: currentlyRunningGame, questions})
 })
@@ -77,16 +73,6 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/logout", async (req, res) => {
-	// // If the user is ingame, remove them from the game
-	// if (req.user.inGame) {
-
-	// 	// Backend: Remove ingame status from DB
-	// 	try {await User.updateOne({twitchId: req.user.twitchId}, {inGame: ""})}
-	// 	catch (error) {console.error(error)}
-
-	// 	// Frontend: Update ingame status in session
-	// 	await io.emit('player left', req.user.inGame, req.user);
-	// }
 	req.logout(function(err) {
 		if (err) { return next(err); }
 		res.redirect('/');
@@ -121,52 +107,30 @@ post("/settings", checkAuthenticated, async function(req, res){
 	const bio = req.body.bio;
 	const showBroadcasterType = (req.body["show-broadcaster-type"]) ? true : false;
 
-	// console.log({userID, chatColour, customChatColour, bio, showBroadcasterType});
+	try {
+		const paramsToUpdate = {
+			bio,
+			chatColour,
+			showBroadcasterType,
+			customChatColour
+		};
 
-	// if (req.user.role == "admin") {
-		// const newBanState = (req.body.banstate === "false") ? 1 : 0;
+		const result = await User.updateOne({ _id: userID }, paramsToUpdate);
+		res.send({status: "success", content: "Preferences updated"});
 
+		req.session.passport.user.doc = {
+			...req.session.passport.user.doc, 
+			bio,
+			chatColour,
+			showBroadcasterType,
+			customChatColour
+		};
+		req.session.save();
 
-		try {
-			const paramsToUpdate = {
-				bio,
-				chatColour,
-				showBroadcasterType,
-				customChatColour
-			};
-
-			const result = await User.updateOne({ _id: userID }, paramsToUpdate);
-			res.send({status: "success", content: "Preferences updated"});
-
-			req.session.passport.user.doc = {
-				...req.session.passport.user.doc, 
-				bio,
-				chatColour,
-				showBroadcasterType,
-				customChatColour
-			};
-			// req.session.passport.user.doc.bio="replaced manually";
-			req.session.save();
-
-			// console.log(result);
-		} catch (error) {
-			res.send({status: "danger", content: "Shit is fucked yo"});
-			console.error(error);
-		}
-
-		// if (result.matchedCount !== 1) {
-		// 	res.send({status: "danger", content: "User with ID " + userID + " not found!"});
-		// }
-		
-		// // if (result.modifiedCount == 0) {
-		// 	res.send({status: "success", content: JSON.stringify(req.body, 4)});
-		// } else {
-			// res.send({status: "failure", content: createErrorHTML(errors)});
-			// }
-		// res.redirect("/admin/users")
-	// } else {
-		// res.redirect("/login")
-	// }
+	} catch (error) {
+		res.send({status: "danger", content: "Shit is fucked yo"});
+		console.error(error);
+	}
 });
 
 router.get("/nologin", async function(req, res){
@@ -175,12 +139,10 @@ router.get("/nologin", async function(req, res){
         try {
             // Find a user with the target Twitch ID
             const user = await User.findOne({ twitchId: req.query.twitchId });    
-            // If the user exists
             if (user) {
                 req.session.userId = user._id; // Store user ID in the session
                 return res.redirect('/profile');
             } else {
-                // No user found with the provided Twitch ID
                 res.redirect('/login');
             }
         } catch (error) {
@@ -191,70 +153,6 @@ router.get("/nologin", async function(req, res){
         const allUsersResult = await User.find({}).collation({ locale: 'en', strength: 2 }).sort({ displayName: 1 }); // Sort case-insensitive
         res.render("nologin", {allUsers: allUsersResult});
     }
-});
-
-// router.get("/nologin", async function(req, res){
-// 	// Check the twitchId query param
-// 	if (req.query.twitchId) {
-// 		try {
-//             // Find a user with the target Twitch ID
-//             const user = await User.findOne({ twitchId: req.query.twitchId });	
-//             // If the user exists
-//             if (user) {
-//                 req.login(user, function(err) {
-//                     if (err) {
-//                         console.log(err);
-//                         return res.redirect('/login');
-//                     }
-
-//                     // Redirect the user to their dashboard or another page
-//                     return res.redirect('/dashboard');
-//                 });
-//             } else {
-//                 // No user found with the provided Twitch ID
-//                 res.redirect('/login');
-//             }
-// 		} catch (error) {
-// 			console.error(error);
-// 		}
-// 	} else {
-// 		const allUsersResult = await User.find({}).collation({ locale: 'en', strength: 2 }).sort({ displayName: 1 }); // Sort case-insensitive
-// 		res.render("nologin", {allUsers: allUsersResult});
-// 	}
-// });
-
-router.get("/nologin2", async function(req, res){
-	if (req.user.role == "admin") {
-		try {
-            // Find a user with the target Twitch ID
-            const result = await User.findOne({ twitchId: req.params.targetTwitchId });	
-            // If the user exists
-            if (result !== null) {
-                // Store the current admin user's data
-                const adminUser = req.session.passport.user.doc;
-                // Update the session user data with the target user's data, and add adminLogin and adminUser properties
-                req.session.passport.user.doc = {...result._doc, adminLogin: true, adminUser};
-
-                // If the session user's displayName matches the target user's displayName and adminUser exists
-                if (req.session.passport.user.doc.displayName === result.displayName
-                    && adminUser) {
-                    // Log a success message and flash a success message to the user
-                    console.log("Successfully logged in as " + result.displayName + "!");
-                    req.flash("success", "Logged in as " + result.displayName);
-                    // Save the session
-                    req.session.save();
-                } else {
-                    // If the displayName doesn't match or adminUser doesn't exist, flash an error message to the user
-                    req.flash("error", "Unable to log in as " + result.displayName);
-                }
-            }
-		} catch (error) {
-			console.error(error);
-		}
-		res.redirect("/admin/users")
-	} else {
-		res.redirect("/login")
-	}
 });
 
 router.post("/updateSession", checkAuthenticated, async function(req, res){
