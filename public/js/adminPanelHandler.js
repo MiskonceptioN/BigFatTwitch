4e1d08715df0fa7bb564075439f9e262d79003ee
Danny Valentine <social@dannyvalz.com>
2025-07-24 22:37:33 +0100

    Add relevant comment

--- public/js/adminPanelHandler.js
+++ public/js/adminPanelHandler.js
@@ -1,4 +1,6 @@
 const defaultImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII=";
+
+// Navigation and utility button handlers
 $("#previous-round").on("click", function(){navigateRound("previous")});
 $("#next-round").on("click", function(){navigateRound("next")});
 $("#blank-answers").on("click", function(){sendEmptyAnswers()});
